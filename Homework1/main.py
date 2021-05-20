a1f447b85fdf360d0ee1ca6b8c3ed88f460b0dce
arpithageorge <arpithageorge@gmail.com>
2021-05-20 15:37:38 +0530

    Homework1: Variables

---
+++ Homework1/main.py
@@ -0,0 +1,19 @@
+Artist = "Backstreet Boys"
+Album = "Millennium"
+Publisher = "Jeff Recordings"
+YearReleased = 1999
+Language = "English"
+Writer = "Max Martin"
+Genre	= "Pope"
+Duration = 3.54
+Nominations = "Grammy Award for Best Vocal Performance by a Double or Multiple Pop Band"
+
+print(Artist)
+print(Album)
+print(Publisher)
+print(YearReleased)
+print(Language)
+print(Writer)
+print(Genre)
+print(Duration)
+print(Nominations)
