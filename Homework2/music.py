466648917197793a2fdfa7febac1edf24a986ee8
arpithageorge <arpithageorge@gmail.com>
2021-05-20 15:38:03 +0530

    Homework2: Functions

---
+++ Homework2/music.py
@@ -0,0 +1,21 @@
+# fuction
+
+def genre():
+	musicGenre = "pope"
+	return musicGenre
+
+def artist():
+	musicArtist = "Backstreet Boys"
+	return musicArtist
+
+def year():
+	musicYear = 1999
+	return musicYear
+
+Genre = genre()
+Artist = artist()
+Year = year()
+
+print("Genre :",Genre)
+print("Artist :",Artist)
+print("Year :",Year)
